0c4220deb7c920600cc8233c38711d99afb05bd4
Muzi <ST10128054@rcconnect.edu.za>
2023-04-25 09:07:41 +0000

    Initialized variables to store information, and created getters and setters for said variables.

--- ST10128054PROG6221POE/Recipe.cs
+++ ST10128054PROG6221POE/Recipe.cs
@@ -8,6 +8,67 @@ namespace ST10128054PROG6221POE
 {
     public class Recipe
     {
-        
+        private int numIngr;
+        private String ingrName;
+        private int ingrQuant;
+        private String measurement;
+        private int numSteps;
+        private String stepDesc;
+
+
+        public int getNumIngr()
+        {
+            return this.numIngr;
+        }
+        public void setNumIngr(int numIngr)
+        {
+            this.numIngr = numIngr;
+        }
+
+        public String getIngrName()
+        {
+            return this.ingrName;
+        }
+        public void setIngrName(String ingrName)
+        {
+            this.ingrName = ingrName;
+        }
+
+        public int getIngrQuant()
+        {
+            return this.ingrQuant;
+        }
+        public void setIngrQuant(int ingrQuant)
+        {
+            this.ingrQuant = ingrQuant;
+        }
+
+        public String getMeasurement()
+        {
+            return this.measurement;
+        }
+        public void setMeasurement(String measurement)
+        {
+            this.measurement = measurement;
+        }
+
+        public int getNumSteps()
+        {
+            return this.numSteps;
+        }
+        public void setNumSteps(int numSteps)
+        {
+            this.numSteps = numSteps;
+        }
+
+        public String getStepDesc()
+        {
+            return this.stepDesc;
+        }
+        public void setStepDesc(String stepDesc)
+        {
+            this.stepDesc = stepDesc;
+        }
+
     }
 }

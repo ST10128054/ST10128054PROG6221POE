410987a2c5e80bf1ad27777a53312e23a509458e
Muzi <ST10128054@rcconnect.edu.za>
2023-04-25 08:45:21 +0000

    Initializing arrays to store the recipe information.

--- ST10128054PROG6221POE/Program.cs
+++ ST10128054PROG6221POE/Program.cs
@@ -8,55 +8,17 @@ namespace ST10128054PROG6221POE
 {
     internal class Program
     {
-        public static int numIngr;
-        public static String ingrName;
-        public static int ingrQuant;
-        public static String measurement;
-        public static int numSteps;
-        public static String stepDesc;
+        
 
         public static String[] ingrNameArr;
         public static int[] ingrQuantArr;
         public static String[] measurementArr;
         public static String[] stepDescArr;
+
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter the number of ingredients for the recipe: ");
-            numIngr = Convert.ToInt32(Console.ReadLine());
-
-            ingrNameArr = new string[numIngr];
-            ingrQuantArr = new int[numIngr];
-            measurementArr = new string[numIngr];
-            stepDescArr = new string[numIngr];
-
-            for (int i = 0; i < numIngr; i++)
-            {
-                Console.WriteLine("Enter the name of ingredient " + i + ": ");
-                ingrName = Console.ReadLine();
-                ingrNameArr[i] = ingrName;
-                Console.WriteLine("Enter the quantity of ingredient " + i + ": ");
-                ingrQuant = Convert.ToInt32(Console.ReadLine());
-                ingrQuantArr[i] = ingrQuant;
-                Console.WriteLine("Enter the unit of measurement of ingredient " + i + ": ");
-                measurement = Console.ReadLine();
-                measurementArr[i] = measurement;
-            }
-
-            Console.WriteLine("Enter the number of steps for the recipe: ");
-            numSteps = Convert.ToInt32(Console.ReadLine());
-
-            StringBuilder sb = new StringBuilder();
-
-            for (int k = 0; k < 1; k++)
-            {
-                for (int j = 0; j < numSteps; j++)
-                {
-                    Console.WriteLine("Enter the description for step " + j + ": ");
-                    stepDesc = Console.ReadLine();
-                    sb.Append(stepDesc);
-                }
-                stepDescArr[k] = sb.ToString();
-            }
+            
             
             
         }

8682cc50916caeccbef9ef329444bfb421a3f4ca
Muzi <ST10128054@rcconnect.edu.za>
2023-04-26 17:09:00 +0000

    Added comments.

--- ST10128054PROG6221POE/Program.cs
+++ ST10128054PROG6221POE/Program.cs
@@ -9,23 +9,26 @@ namespace ST10128054PROG6221POE
 {
     internal class Program
     {
+        //initialzing an object of the class Recipe
         public static Recipe rc = new Recipe();
-        public static ArrayList ingrNameArr = new ArrayList();
-        public static ArrayList ingrQuantArr = new ArrayList();
-        public static ArrayList scaledQuantArr = new ArrayList();
-        public static ArrayList measurementArr = new ArrayList();
-        public static ArrayList stepDescArr = new ArrayList();
+        //initialzing arraylist to store the recipe details
+        public static ArrayList ingrNameArr = new ArrayList();//stores name of ingredient
+        public static ArrayList ingrQuantArr = new ArrayList();//stores the quantity of ingredient before scaling
+        public static ArrayList scaledQuantArr = new ArrayList();//stores the quantity of ingredient after scaling
+        public static ArrayList measurementArr = new ArrayList();//stores the unit of measurement
+        public static ArrayList stepDescArr = new ArrayList();//stores the descriptions of the steps
         public static int mainMenu = 0;
         public static Boolean scaled = false;
 
 
         static void Main(string[] args)
         {
-            
-
+            //while loop to keep the aplication running until the user wants to stop
             while(mainMenu < 6)
             {
+                //change text colour to green
                 Console.ForegroundColor = ConsoleColor.Green;
+                //menu system for navigation to access functions
                 Console.WriteLine("************************************\n" +
                                   "Recipe App\n" +
                                   "1) Enter ingredients and steps.\n" +
@@ -33,27 +36,32 @@ namespace ST10128054PROG6221POE
                                   "3) Reset measurements.\n" +
                                   "4) Display recipe.\n" +
                                   "5) Clear recipe.\n" +
-                                  "6) Exit.");
+                                  "6) Exit.(enter 6 twice)");
                 mainMenu = Convert.ToInt32(Console.ReadLine());
-
+                //series of else if statements to select the desired method
                 if (mainMenu == 1)
                 {
+                    //call enterRecipe method
                     enterRecipe();
                 }
                 else if (mainMenu == 2)
                 {
+                    //call scaleMeasurement method
                     scaleMeasurement();
                 }
                 else if (mainMenu == 3)
                 {
+                    //call resetMeasurement method
                     resetMeasurement();
                 }
                 else if (mainMenu == 4)
                 {
+                    //call displayRecipe method
                     displayRecipe();
                 }
                 else if (mainMenu == 5)
                 {
+                    //call clearRecipe method
                     clearRecipe();
                 }
             }
@@ -61,9 +69,11 @@ namespace ST10128054PROG6221POE
             Console.ReadLine();
         }
 
+        //this method allows the user the enter the details of the recipe and stores them in arraylists
         public static void enterRecipe()
         {
             Console.ForegroundColor = ConsoleColor.Cyan;
+            //prompting user for information and then storing said information into arraylists
             Console.WriteLine("************************************\n" + 
                               "Enter the number of ingredients for the recipe: ");
             rc.NumIngr = Convert.ToInt32(Console.ReadLine());
@@ -95,12 +105,15 @@ namespace ST10128054PROG6221POE
             stepDescArr.Add(sb.ToString());
 
             Console.ForegroundColor = ConsoleColor.Yellow;
+            //providing the user with feedback after successfull function
             Console.WriteLine("RECIPE SAVED.");
         }
 
+        //this method scales the quantities of the ingredients
         public static void scaleMeasurement()
         {
             Console.ForegroundColor = ConsoleColor.Magenta;
+            //menu system to allow the user to pick the factor amount
             Console.WriteLine("************************************\n" + 
                               "Do you want to alter the quantities of the ingredients?\n" +
                               "1) Half.\n" +
@@ -108,6 +121,7 @@ namespace ST10128054PROG6221POE
                               "3) Triple.\n");
             rc.Alter = Convert.ToInt32(Console.ReadLine());
 
+            //multiplies the amount in the arraylist by the factor and stores it in a new arraylist
             if (rc.Alter == 1)
             {
                 for (int i = 0; i < ingrQuantArr.Count; i++)
@@ -139,16 +153,19 @@ namespace ST10128054PROG6221POE
             Console.WriteLine("MEASUREMENTS SCALED.");
         }
 
+        //this method resets the quantities back to their original values
         public static void resetMeasurement()
         {
             Console.ForegroundColor = ConsoleColor.Yellow;
             for (int i = 0; i < ingrQuantArr.Count; i++)
             {
+                //resets quatities to orignal values
                 scaledQuantArr.Insert(i, ingrQuantArr[i]);
             }
             Console.WriteLine("MEASUREMENTS RESET.");
         }
 
+        //this method displays the recipe information
         public static void displayRecipe() 
         {
             Console.ForegroundColor = ConsoleColor.Red;
@@ -177,6 +194,7 @@ namespace ST10128054PROG6221POE
             }
         }
 
+        //this method clears the recipe information
         public static void clearRecipe()
         {
             ingrNameArr.Clear();
